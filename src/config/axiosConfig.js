58accbd8dca8960dd6ba24a1b46d95749141114b
Eliab Lopez Murillo <elopez@heinsohn.com.co>
2024-03-16 17:06:39 -0500

    Change api context

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 // Development
-const baseURL = 'http://localhost:4000/api/v1';
+// const baseURL = 'http://localhost:4000/api/v1';
 
 // Production
-// const baseURL = 'https://shoe-store-server-c2ok.onrender.com/api/v1';
+const baseURL = 'https://shoe-store-server-c2ok.onrender.com/api/v1';
 
 export const axiosInstance = axios.create({
   baseURL,
